bcca01bf052f7e525b66bd236549f5ece9d4727b
Trinadh <133766681+3nadh3@users.noreply.github.com>
2025-10-20 21:16:01 -0400

    Delete Backend directory

--- Backend/src/models/Visitor.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('../config/db');
-
-const visitorSchema = new mongoose.Schema({ count: Number });
-const Visitor = mongoose.model("Visitor", visitorSchema);
-
-module.exports = Visitor;
-
--- Backend/src/routes/VistorRoutes.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-const VistorNumber = require('../controllers/VistorController');  // ✅ Fix: Import function, not file
-const router = express.Router();
-
-router.get('/visitor', VistorNumber);  // ✅ Fix: Pass the function, not an object
-
-module.exports = router;
